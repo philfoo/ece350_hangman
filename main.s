e53419ad59ab3e56bd9c6b899a88f7e975782c80
Phil <philphoo95@gmail.com>
2016-04-23 11:55:10 -0400

    a couple more thoughts

--- main.s
+++ main.s
@@ -1,8 +1,9 @@
 ##--------------------------------------------Register Definition-------------------------------------------##
 ## $r0 = 0
-## $r1 = 1
+## $r1 = 1 always
 ## $r2: will hold the length of the word
 ## $r3: loop counter for each character of word (i)
+## $r4 is the temporary register where you check it against data
 ## $r30: where the input from keyboard ends up
 
 
@@ -23,6 +24,10 @@ get_characters:
 jal wait_for_input             ##Wait for each character
 
 ##TODO: store character from register 30 somewhere
+add $r4, $r0, $r30             ##Move character to register four
+add $r30, $r0, $r0             ##Set input register back to 0
+jal render_character           ##Go to render character method
+
 
 addi $r3, $r3, 1               ##Increment counter
 blt $r3, $r2, get_characters   ##Go back through for-loop because $r3 < length of word
@@ -43,3 +48,33 @@ jump wait_for_input            ##loop back, waiting
 input_ready:
 addi $r30, $r0, 0              ##set $r30 back to 0 for the next time wait_for_input gets called
 jr $r31                        ##Go back to whatever called this
+
+
+##-----------------------------------Creating characters section-----------------------------------------------##
+render character:
+lw $r2, 0($r0)                 #load A
+nop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
